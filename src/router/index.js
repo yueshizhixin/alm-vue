965df4e335196ee3bc41f4b2bee76e6a49af6bed
yueshizhixin <yueshizhixin@163.com>
2018-06-09 18:01:44 +0800

    vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 
-import HelloWorld from '@/components/HelloWorld'
+import vueContainer from '../components/vueContainer'
 import test1 from '../components/test/test1'
 import test2 from '../components/test/test2'
 import t1Show from '../components/test/test1/show'
 import t1Update from '../components/test/test1/update'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
       component:
-        HelloWorld
+      vueContainer
 
     },
     {
@@ -24,11 +23,11 @@ export default new Router({
       name: 'test1',
       component: test1,
       children: [{
-        path: '/show',
+        path: 'show',
         component: t1Show,
       },
         {
-          path: '/update',
+          path: 'update',
           component: t1Update,
         },
       ],
